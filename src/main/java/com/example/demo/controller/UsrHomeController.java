f5f80bca90b83e4bc7e94f1cf7f4c1a9a6c292ab
ryuj7338 <ryuj7338@gmail.com>
2025-05-07 10:23:39 +0900

    실행 환경 세팅

---
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UsrHomeController {
+	@RequestMapping("/usr/home/main")
+	@ResponseBody
+	public String ShowMain() {
+		return "집가고 싶다";
+	}
+}
