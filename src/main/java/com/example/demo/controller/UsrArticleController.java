6cf0dda60cb232b4df0ecdd83c55ce9fe52e917b
ryuj7338 <ryuj7338@gmail.com>
2025-05-07 14:26:09 +0900

    게시글 삭제 기능 추가

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -28,10 +28,30 @@ public class UsrArticleController {
 		testData();
 	}
 	
+	// 서비스 메서드
+	private void testData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "탄지로";
+			String body = "물의 호흡 제"+i+"형";
+			
+			writeArticle(title, body);
+		}
+		
+	}
 	
-	@RequestMapping("/usr/article/doAdd")
+	private Article getArticleId(int id) {
+		for (Article article : articles) {
+			if(article.getId() == id) {
+				return article;
+			}
+		}
+		return null;
+	}
+	
+	// 액션 메서드
+	@RequestMapping("/usr/article/writeArticle")
 	@ResponseBody
-	public Article doAdd(String title, String body) {
+	public Article writeArticle(String title, String body) {
 		
 		int id = lastArticleId + 1;
 		
@@ -45,32 +65,25 @@ public class UsrArticleController {
 		return article;
 	}
 	
-	private void testData() {
-		for (int i = 1; i <= 10; i++) {
-			String title = "탄지로";
-			String body = "물의 호흡 제"+i+"형";
-			
-			doAdd(title, body);
+	
+	@RequestMapping("/usr/article/doDelete")
+	@ResponseBody
+	public String doDelete(int id) {
+		
+		Article article = getArticleId(id);
+		
+		
+		if (article == null) {
+			return id+"번 글은 없습니다.";
 		}
 		
+		articles.remove(id);
+		
+		
+		return id+"번 글이 삭제되었습니다.";
 	}
-//	
-//	@RequestMapping("/usr/article/doDelete")
-//	@ResponseBody
-//	public Article doDelete() {
-//		
-//		Article foundArticle = null;
-//		
-//		for(Article foundArtcle : articles) {
-//			if(article.getID() == id) {
-//				foundArticle = article;
-//			}
-//		}
-//		
-//		
-//		return article;
-//	}
 	
+
 
 	@RequestMapping("/usr/article/getArticles")
 	@ResponseBody
