4b8e2bfe6991b6324efccea3beb435993745e761
ryuj7338 <ryuj7338@gmail.com>
2025-05-08 14:32:44 +0900

    입력된 데이터의 null 체크 함수

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.MemberRepository;
+import com.example.demo.util.Ut;
 import com.example.demo.vo.Member;
 
 @Service
@@ -12,34 +13,31 @@ public class MemberService {
 	@Autowired
 	private MemberRepository memberRepository;
 
-	
 //	서비스 메서드
 	public MemberService(MemberRepository memberRepository) {
 
 		this.memberRepository = memberRepository;
-		
+
 	}
-	
-	
+
 	public int doJoin(String loginId, String loginPw, String name) {
-		
+
 		Member existsMember = getMemberLoginId(loginId);
-		System.out.println("existsMember: "+ existsMember);
-		
-		if(existsMember != null) {
+		System.out.println("existsMember: " + existsMember);
+
+		if (existsMember != null) {
 			return -1;
 		}
-		
-		
+
 		memberRepository.doJoin(loginId, loginPw, name);
-		
+
 		return memberRepository.getLastInsertId();
 	}
-	
+
 	private Member getMemberLoginId(String loginId) {
 		return memberRepository.getMemberLoginId(loginId);
 	}
-	
+
 	public Member getMemberId(int id) {
 
 		return memberRepository.getMemberId(id);
