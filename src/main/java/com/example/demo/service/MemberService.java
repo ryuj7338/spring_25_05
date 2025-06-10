40e3e62ee753d45810ed7a536a86bbdb164c70d3
ryuj7338 <ryuj7338@gmail.com>
2025-06-10 16:39:43 +0900

    SMTP, 임시비밀번호 이메일로 전송

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.repository.MemberRepository;
@@ -11,8 +12,16 @@ import com.example.demo.vo.ResultData;
 @Service
 public class MemberService {
 
+	@Value("${custom.siteMainUri}")
+	private String siteMainUri;
+	@Value("${custom.siteName}")
+	private String siteName;
+	
 	@Autowired
 	private MemberRepository memberRepository;
+	
+	@Autowired
+	private MailService mailService;
 
 //	서비스 메서드
 	public MemberService(MemberRepository memberRepository) {
@@ -20,7 +29,30 @@ public class MemberService {
 		this.memberRepository = memberRepository;
 
 	}
+	
+	public ResultData notifyTempLoginPwByEmail(Member actor) {
+		String title = "[" + siteName + "] 임시 패스워드 발송";
+		String tempPassword = Ut.getTempPassword(6);
+		String body = "<h1>임시 패스워드 : " + tempPassword + "</h1>";
+		body += "<a href=\"" + siteMainUri + "/usr/member/login\" target=\"_blank\">로그인 하러가기</a>";
 
+		ResultData sendResultData = mailService.send(actor.getEmail(), title, body);
+
+		if (sendResultData.isFail()) {
+			return sendResultData;
+		}
+
+		setTempPassword(actor, tempPassword);
+
+		return ResultData.from("S-1", "계정의 이메일주소로 임시 패스워드가 발송되었습니다.");
+	}
+	
+
+	private void setTempPassword(Member actor, String tempPassword) {
+		memberRepository.modify(actor.getId(), Ut.sha256(tempPassword), null, null, null, null);
+	}
+
+	
 	public ResultData<Integer> join(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 
 		Member existsMember = getMemberLoginId(loginId);
@@ -43,20 +75,24 @@ public class MemberService {
 		return ResultData.from("S-1","회원가입을 성공하였습니다.", "가입 성공 id", id);
 	}
 
+	
 	public Member getMemberByNameAndEmail(String name, String email) {
 		
 		return memberRepository.getMemberByNameAndEmail(name, email);
 	}
 
+	
 	public Member getMemberLoginId(String loginId) {
 		return memberRepository.getMemberLoginId(loginId);
 	}
 
+	
 	public Member getMemberId(int id) {
 
 		return memberRepository.getMemberId(id);
 	}
 
+	
 	public ResultData modify(int loginedMemberId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
 
 		loginPw = Ut.sha256(loginPw);
@@ -66,10 +102,12 @@ public class MemberService {
 		return ResultData.from("S-1", "회원정보 수정 완료");
 	}
 
+	
 	public ResultData modifyWithoutPw(int loginedMemberId, String name, String nickname, String cellphoneNum,
 			String email) {
 		memberRepository.modifyWithoutPw(loginedMemberId, name, nickname, cellphoneNum, email);
 
 		return ResultData.from("S-1", "회원정보 수정 완료");
 	}
+	
 }
